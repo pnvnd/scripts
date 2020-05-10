d4309a14e687f8b82364c653d4620bf95f19dd4a
pnvnd <pvnn@msn.com>
2020-05-10 01:05:52 -0400

    Initial commit

---
+++ powershell/subdirectory_sizes.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem -Force | 
+Where-Object { $_.PSIsContainer } | 
+ForEach-Object { $_.Name + ": " + "{0:N2}" -f ((Get-ChildItem $_ -Recurse -Force | Measure-Object Length -Sum -ErrorAction SilentlyContinue).Sum / 1MB) + " MB" }
