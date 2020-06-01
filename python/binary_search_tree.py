db093b325276287bc0c48b674dad7001f4653582
pnvnd <peter@datacrunch.ca>
2020-05-31 22:02:22 -0400

    Binary Search Tree Example

---
+++ python/binary_search_tree.py
@@ -0,0 +1,24 @@
+#Definition for a binary tree node.
+class TreeNode:
+    def __init__(self,x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(node, lower, upper):
+            if not node:
+                return True
+
+        val = node.val
+        if val <= lower or val >= upper:
+            return False
+
+        if not helper(node.right, val, upper):
+            return False
+        if not helper(node.left,lower, val):
+            return False
+        return True
+
+    return helper(root, float('-inf'), float('inf'))
