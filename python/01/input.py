509235aeb537f388ddcc30eb1ed5160b183adfa7
pnvnd <peter@datacrunch.ca>
2020-05-31 23:08:52 -0400

    Python in Easy Steps - Chapter 01

---
+++ python/01/input.py
@@ -0,0 +1,13 @@
+# Initialize a variable with a user-specified value.
+print("I'm Zero, what's your name?")
+user = input( ">> " )
+
+# Output a string and a variable value.  Use + to concatenate or use , to add space before string.
+print( "Welcome, " + user + "!")
+
+# Initialize another variable with a user-specified value.
+print("Favorite programming language?") 
+lang = input( ">> " )
+
+# Output a string and a variable value.
+print( lang , "Is" , "Fun" , sep = " * " , end = "!\n" )
---
+++ python/01/var.py
@@ -0,0 +1,18 @@
+"""Block Comments with 3 double-quotes"""
+'''Or block comment with 3 single-quotes'''
+
+# Initialize a variable with an integer value.
+var = 8
+print(var)
+
+# Assign a float value to the variable.
+var = 3.142
+print(var)
+
+# Assign a string value to the variable.
+var = "Python in easy steps"
+print(var)
+
+# Assign a boolean value to the variable.
+var = True
+print(var)
