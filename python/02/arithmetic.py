52cdbd9c71c41082ba4841380436403d901dba9b
pnvnd <peter@datacrunch.ca>
2020-06-02 00:19:15 -0400

    Initial commit

---
+++ python/02/arithmetic.py
@@ -0,0 +1,12 @@
+a = 8
+b = 2
+
+print("Addition:\t", a, "+", b, "=", a+b)
+print("Subtraction:\t", a, "-", b, "=", a-b)
+print("Multiplication:\t", a, "x", b, "=", a*b)
+print("Division:\t", a, "/", b, "=", a/b)
+
+# Floor division divides and truncates decimal
+print("Floor Division:\t", a, "/", b, "=", a//b)
+print("Modulo:\t\t", a, "%", b, "=", a%b)
+print("Exponent:\t", a, "^", b, "=", a**b)
