a571577baa1bd55a0f0c3a621e68e7ac9ed46045
pnvnd <peter@datacrunch.ca>
2020-06-02 23:16:12 -0400

    Initial commit

---
+++ python/03/dictionary.py
@@ -0,0 +1,12 @@
+dict = { 'name' : 'Bob' , 'ref' : 'Python' , 'sys' : 'Win' }
+print( 'Dictionary:' , dict )
+
+print( '\nReference:' , dict[ 'ref' ] )
+
+print( '\nKeys:' , dict.keys() )
+
+del dict[ 'name' ]
+dict[ 'user' ] = 'Tom'
+print( '\nDictionary:' , dict )
+
+print( '\nIs There A name Key?:' ,'name' in dict )
---
+++ python/03/pop.py
@@ -0,0 +1,17 @@
+basket = [ 'Apple' , 'Bun' , 'Cola' ]
+crate = [ 'Egg' , 'Fig' , 'Grape' ]
+
+print( 'Basket List:' , basket )
+print( 'Basket Elements:' , len( basket ) )
+
+basket.append( 'Damson' )
+print( 'Appended:' , basket )
+print( 'Last Item Removed:' , basket.pop() )
+print( 'Basket List:' , basket )
+
+basket.extend( crate )
+print( 'Extended:' , basket )
+del basket[1]
+print( 'Item Removed:' , basket )
+del basket[1:3]
+print( 'Slice Removed:' , basket )
