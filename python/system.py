78ead5f4d05ff0ce0bba460361b7a883027a046e
pnvnd <peter@datacrunch.ca>
2020-06-06 14:01:40 -0400

    Initial commit

---
+++ python/system.py
@@ -0,0 +1,14 @@
+import sys, keyword
+# import keyword
+
+print( 'Python Version:' , sys.version )
+
+print( 'Python Interpreter Location:' , sys.executable )
+
+print( 'Python Module Search Path: ' )
+for dir in sys.path :
+    print( dir )
+
+print( 'Python Keywords: ' )
+for word in keyword.kwlist :
+    print( word )
