b62f5fa98ed2603f9b61e681fdbb25b8be3dcdb2
pnvnd <peter@datacrunch.ca>
2020-07-04 10:12:42 -0400

    Update recursion.py

--- python/recursion.py
+++ python/recursion.py
@@ -1,13 +1,22 @@
-# Using recursion to implement power and factorial
-
+# Using recursion to implement base**exponent
 def power(num, pwr):
-	if pwr = 0:
+	if pwr == 0:
 		return 1
 	else:
 		return num * power(num, pwr-1)
 
-def factorial(num)
+# Using recursion to implement factorial, where 0! = 1 by definition. 3! = 3*2*1
+def factorial(num):
 	if num == 0:
 		return 1
 	else:
 		return num * factorial(num -1)
+
+# Using recursion to implement the Fibonacci sequence
+def fibonacci(n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
