b402b2292be28043079a385be920ddb4aa5f5e11
pnvnd <peter@datacrunch.ca>
2020-07-03 23:07:48 -0400

    Create recursion.py

---
+++ python/recursion.py
@@ -0,0 +1,13 @@
+# Using recursion to implement power and factorial
+
+def power(num, pwr):
+	if pwr = 0:
+		return 1
+	else:
+		return num * power(num, pwr-1)
+
+def factorial(num)
+	if num == 0:
+		return 1
+	else:
+		return num * factorial(num -1)
