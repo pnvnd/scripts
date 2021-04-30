0b0c9c3ae7c2a3f4991158e06d62f2c90df18d1e
Peter Nguyen <peter@datacrunch.ca>
2021-04-30 19:58:58 -0400

    Rounded temperature to 2 decimal places

--- python/temp_convert.py
+++ python/temp_convert.py
@@ -1,20 +1,22 @@
+#from decimal import *
+
 '''Temperature Converter'''
 
-print("Convert to 'C' or 'F'?")
+print("Convert to '°C' or '°F'?")
 deg = input(">> ")
 loop = "Y"
 
-while (loop == "Y" or loop =="y"):
+while (loop.lower() == "y"):
     try:
         # C=5/9*(F-32)
-        if deg == "C" or deg == "c":
+        if deg.upper() == "C":
             temp = input("Enter Fahrenheit temperature: ")
-            print(temp, "F is", 5/9*(int(temp)-32), "C")
+            print(temp, "°F is", round(5/9*(int(temp)-32),2), "°C")
 
         # F=(9/5*C)+32
-        elif deg == "F" or deg == "f":
+        elif deg.upper() == "F":
             temp = input("Enter Celsius temperature: ")
-            print(temp, "C is", 9/5*int(temp)+32, "F")
+            print(temp, "°C is", round(9/5*int(temp)+32,2), "°F")
 
         else:
             print("Invalid input!")
@@ -22,8 +24,7 @@ while (loop == "Y" or loop =="y"):
     except ValueError:
             print("Enter a NUMBER, try again.")
     else:
-        loop = input("Again? [Y/n]: ")
-
+        loop = input("Again? [Y/n]: ").upper()
 
 
 
